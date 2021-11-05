5596ec9014be9b78bce37a1f1bc0441392353dea
Sergei <80399042+serjcomp@users.noreply.github.com>
2021-11-06 01:46:44 +0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,30 @@
+from blockchain2 import block
+
+b = block()
+flag = 0
+
+def set_flag_menu():
+    menu_flag = int(input("введи номер: 1 - настройка, 0 - майнинг"))
+    return menu_flag
+    
+flag = set_flag_menu()
+
+private_key = 0
+public_key = 0
+while True:
+    if flag == 1:
+        private_key = b.generate_private_key()
+        public_key = b.generate_public_key(private_key)
+        flag = 0
+    if flag == 0:
+        b.open_block()
+        b.input_text()
+        b.sign_block(private_key)
+        b.edit_transaction()
+        b.previos_hash()
+        b.sign_block(private_key)
+        b.edit_nonse()
+        b.hash_block()
+        b.write()
+    b.sbros()
+    print("БЛОК СОЗДАН")
